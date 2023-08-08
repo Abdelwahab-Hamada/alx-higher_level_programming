7270993ca80181992994c974d8d5077be3571b7c
root <abdelwahab.hamada@gmail.com>
2023-08-08 04:14:51 -0700

    8final

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+def to_upper(char):
+    if ord(char) >= 97 and ord(char) <= 122:
+        return (ord(char) - 32)
+    else:
+        return ord(char)
+
+
 def uppercase(str):
-    new_str=""
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = ord(c) - 32
-        else:
-            c = ord(c)
-        new_str += f"{c:c}"
-    print(new_str)
+    new_str = ""
+    for char in str:
+        new_str += "%c" % to_upper(char)
+    print("{:s}".format(new_str))
