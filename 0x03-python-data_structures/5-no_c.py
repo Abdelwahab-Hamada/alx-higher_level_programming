be2154bcc51717b49c9ffd9546c9d9e4cf9445af
root <abdelwahab.hamada@gmail.com>
2023-08-12 20:05:00 -0700

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate(str.maketrans("cC",""))
+    new_string = my_string.translate({"c": None, "C": None})
     return new_string
