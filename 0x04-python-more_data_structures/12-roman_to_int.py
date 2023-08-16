150dca657acd987411e2a65185c0aabd26748a2f
root <abdelwahab.hamada@gmail.com>
2023-08-16 03:33:09 -0700

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
+    rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    return rom_int.get(roman_string)
