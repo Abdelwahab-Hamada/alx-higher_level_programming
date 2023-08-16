538d4cbe7d9ab7e0b3f28e5f23e6a59d3cdb4f3b
root <abdelwahab.hamada@gmail.com>
2023-08-16 03:42:01 -0700

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,4 +5,11 @@ def roman_to_int(roman_string):
 
     rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    return rom_int.get(roman_string)
+    result = rom_int.get(roman_string[0])
+
+    for char in roman_string[1:]:
+        if result < rom_int.get(char):
+            result -= rom_int.get(char)
+        result += rom_int.get(char)
+
+    return result
